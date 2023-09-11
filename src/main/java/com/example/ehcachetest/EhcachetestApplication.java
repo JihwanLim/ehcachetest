2befa53c8f57eb3f551f099c4625d1a0410bc90a
unknown <jihwan6933@gmail.com>
2023-09-12 00:46:56 +0900

    first commit

---
+++ src/main/java/com/example/ehcachetest/EhcachetestApplication.java
@@ -0,0 +1,13 @@
+package com.example.ehcachetest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EhcachetestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EhcachetestApplication.class, args);
+	}
+
+}
